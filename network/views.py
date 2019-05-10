590b45195f1fba3fbb7d51cd8a1ad9bb0b37eccd
SravaniVP <vpsravanitm909@gmail.com>
2019-05-10 21:07:14 +0530

    signup added

---
+++ network/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Welcome to Networking APP.")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'network/signup.html', {'form': form})
